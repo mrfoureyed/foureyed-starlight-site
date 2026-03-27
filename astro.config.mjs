8c45259d3babf0f5b64609bb52c5914baf92de84
Jordan Lee <jlee@foureyed.net>
2026-03-26 21:49:36 -0400

    updated landing page, better do a quick save

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'FE',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mrfoureyed' }],
 			sidebar: [
 				{
 					label: 'Guides',
